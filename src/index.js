186d369fe636e70c220faac8938633695d2d964f
Nicholas <nicholasjoven@gmail.com>
2022-07-06 13:18:33 -0400

    commit tree acc0f006729224136d89fb3fd9b5cf72930944af

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form')
 
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.isPrototypeOf)
+        console.log(event.target.searchByID.value)
+        const input = event.target.searchByID.value
+
+        let inputs = document.getElementsByTagName('input')
+        console.log(inputs)
+        console.log(inputs.searchByID.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4')
+                const summary = document.querySelector('section#movieDetails p')
+
+                title.innerText = data.title
+                summary.innerText = data.summary
+            })
+    })
+}
 document.addEventListener('DOMContentLoaded', init);
